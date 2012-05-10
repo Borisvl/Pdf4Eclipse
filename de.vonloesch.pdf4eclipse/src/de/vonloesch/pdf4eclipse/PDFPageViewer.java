7126689b3e3df4a39321a54639320f283b260273
Robert Bamler <robert.bamler@gmail.com>
2012-05-10 22:58:39 +0200

    Proof-of-concept for auto-trimming of page margins
    This implementation follows the second approach outlined
    in #issuecomment-5596872 (see issue #20 on github.com/Borisvl/
    Pdf4Eclipse). Auto-trimming can be turned on and of with the
    boolean field PDFPageView.autoTrimOn, which, for now, is just
    a static constant.
    
    Things that remain to do:
    
     - I'm not sure if this implementation is memory efficient.
       Maybe we could do with copying the image one less time.
    
     - Coordinate transformation for forward/backward search and
       for annotations.
    
     - Correct handling of the "fit page to screen" and "fit page
       width to screen width" zoom actions.
    
     - Frontend to turn auto-trimming on and off (toggle button).

--- de.vonloesch.pdf4eclipse/src/de/vonloesch/pdf4eclipse/PDFPageViewer.java
+++ de.vonloesch.pdf4eclipse/src/de/vonloesch/pdf4eclipse/PDFPageViewer.java
@@ -16,6 +16,7 @@ import java.awt.geom.Rectangle2D;
 import java.awt.geom.Rectangle2D.Double;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
 import java.awt.image.DirectColorModel;
 import java.awt.image.IndexColorModel;
 import java.awt.image.WritableRaster;
@@ -53,6 +54,8 @@ import de.vonloesch.pdf4eclipse.model.IPDFPage;
  *
  */
 public class PDFPageViewer extends Canvas implements PaintListener, IPreferenceChangeListener{
+    private static final boolean autoTrimOn = true;
+
     /** The image of the rendered PDF page being displayed */
     private Image currentImage;
     
@@ -306,6 +309,9 @@ public class PDFPageViewer extends Canvas implements PaintListener, IPreferenceC
 
     	if (sz.x == 0 || sz.y == 0) return;
     	currentImage = page.getImage(newH, newW);
+    	if (autoTrimOn) {
+    		trimCurrentImage();
+    	}
 
     	newW = currentImage.getWidth(null);
     	newH = currentImage.getHeight(null);
@@ -329,6 +335,78 @@ public class PDFPageViewer extends Canvas implements PaintListener, IPreferenceC
     	}
     }
 
+    private void trimCurrentImage() {
+    	if (currentImage==null)
+    		return;
+    	
+    	int origw = currentImage.getWidth(null);
+    	int origh = currentImage.getHeight(null);
+    	if (origw<1 || origh<1)
+    		return;
+    	
+    	BufferedImage img = new BufferedImage(origw, origh, BufferedImage.TYPE_INT_ARGB);
+    	img.getGraphics().drawImage(currentImage, 0, 0, origw, origh, 0, 0,	origw, origh, null);
+
+    	int[] srcbuf = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
+
+    	// detect upper margin
+    	int trimy1 = origh;
+    	int referenceColor = srcbuf[0];
+    	for (int i=0; i!=srcbuf.length; i++) {
+    		if (srcbuf[i] != referenceColor) {
+    			trimy1 = i/origw;
+    			break;
+    		}
+    	}
+    	
+    	// detect lower margin
+    	int trimy2 = 0;
+    	referenceColor = srcbuf[srcbuf.length-1];
+    	for (int i=srcbuf.length-1; i!=trimy1*origw-1; i--) {
+    		if (srcbuf[i] != referenceColor) {
+    			trimy2 = i/origw + 1;
+    			break;
+    		}
+    	}
+    	
+    	// detect left margin
+    	int trimx1 = 0;
+    	referenceColor = srcbuf[0];
+    	int offset = trimy1*origw;
+    	int end = trimy2*origw;
+    	trimx1loop:
+    		while (trimx1 != origw) {
+    			for (int i=offset+trimx1; i<end; i+=origw) {
+    				if (srcbuf[i] != referenceColor)
+    					break trimx1loop;
+    			}
+    			trimx1++;
+    		}
+    	
+    	// detect right margin
+    	int trimx2 = origw-1;
+    	referenceColor = srcbuf[srcbuf.length-1];
+    	trimx2loop:
+    		while (trimx2 != trimy1) {
+    			for (int i=offset+trimx2; i<end; i+=origw) {
+    				if (srcbuf[i] != referenceColor)
+    					break trimx2loop;
+    			}
+    			trimx2--;
+    		}
+    	trimx2++;
+    	
+    	// add some margin (2% of the smaller one of the two dimensions) 
+    	int margin = Math.min(trimx2-trimx1, trimy2-trimy1) / 50;
+    	trimx1 = Math.max(0, trimx1-margin);
+    	trimx2 = Math.min(origw, trimx2+margin);
+    	trimy1 = Math.max(0, trimy1-margin);
+    	trimy2 = Math.min(origh, trimy2+margin);
+    	
+    	// crop image
+    	currentImage = img.getSubimage(trimx1, trimy1, trimx2-trimx1, trimy2-trimy1);
+    }
+    
     private Rectangle getRectangle(Rectangle2D r) {
     	return new Rectangle((int)Math.round(r.getX()), (int)Math.round(r.getY()), (int)Math.round(r.getWidth()), (int)Math.round(r.getHeight()));
     }
